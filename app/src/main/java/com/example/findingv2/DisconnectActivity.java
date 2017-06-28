b898c63803fbe23beeb3a4490693fb414de62b7d
jarishing <jarishing@gmail.com>
2017-06-28 16:49:11 +0800

    Detect the product

---
+++ app/src/main/java/com/example/findingv2/DisconnectActivity.java
@@ -0,0 +1,16 @@
+package com.example.findingv2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 6/28/2017.
+ */
+
+public class DisconnectActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.disconnect_view);
+    }
+}
--- app/src/main/java/com/example/findingv2/StartActivity.java
+++ app/src/main/java/com/example/findingv2/StartActivity.java
@@ -1,7 +1,10 @@
 package com.example.findingv2;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -17,18 +20,14 @@ import dji.common.error.DJIError;
 import dji.common.error.DJISDKError;
 import dji.sdk.base.BaseComponent;
 import dji.sdk.base.BaseProduct;
+import dji.sdk.products.Aircraft;
 import dji.sdk.sdkmanager.DJISDKManager;
 
 /**
  * Created by jarishing on 27/6/2017.
  */
 
-public class StartActivity extends AppCompatActivity {
-
-    private static final String TAG = MainActivity.class.getName();
-    public static final String FLAG_CONNECTION_CHANGE = "dji_sdk_connection_change";
-    private static BaseProduct mProduct;
-    private Handler mHandler;
+public class StartActivity extends AppCompatActivity{
 
     private Button Connection_Button;
 
@@ -54,93 +53,68 @@ public class StartActivity extends AppCompatActivity {
 
         setContentView(R.layout.start_view);
 
-        //Initialize DJI SDK Manager
-        mHandler = new Handler(Looper.getMainLooper());
-        DJISDKManager.getInstance().registerApp(this, mDJISDKManagerCallback);
-
         Intial_ui();
-        Onclick();
+
+
+        // Register the broadcast receiver for receiving the device connection's changes.
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(FindingApplication.FLAG_CONNECTION_CHANGE);
+        registerReceiver(mReceiver, filter);
+
+        //Connected_View();
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 
     private void Intial_ui() {
         Connection_Button = (Button) findViewById(R.id.Connection_Button);
     }
 
-    private void Onclick() {
+    private void Connected_View() {
         Connection_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent Connect_to_Drone = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(Connect_to_Drone);
-                StartActivity.this.finish();
+                //StartActivity.this.finish();
             }
         });
     }
 
-    private DJISDKManager.SDKManagerCallback mDJISDKManagerCallback = new DJISDKManager.SDKManagerCallback() {
-        @Override
-        public void onRegister(DJIError error) {
-            Log.d(TAG, error == null ? "success" : error.getDescription());
-            if(error == DJISDKError.REGISTRATION_SUCCESS) {
-                DJISDKManager.getInstance().startConnectionToProduct();
-                Handler handler = new Handler(Looper.getMainLooper());
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(), "Register Success", Toast.LENGTH_LONG).show();
-                    }
-                });
-            } else {
-                Handler handler = new Handler(Looper.getMainLooper());
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(), "register sdk failed, check if network is available", Toast.LENGTH_LONG).show();
-                    }
-                });
+    private void Disconnected_View() {
+        Connection_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Connect_Fail = new Intent(getApplicationContext(), DisconnectActivity.class);
+                startActivity(Connect_Fail);
+                //StartActivity.this.finish();
             }
-            Log.e("TAG", error.toString());
-        }
+        });
+    }
+
+    protected BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
         @Override
-        public void onProductChange(BaseProduct oldProduct, BaseProduct newProduct) {
-            mProduct = newProduct;
-            if(mProduct != null) {
-                mProduct.setBaseProductListener(mDJIBaseProductListener);
-            }
-            notifyStatusChange();
+        public void onReceive(Context context, Intent intent) {
+            Log.e("mss","Test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            refreshSDKRelativeUI();
         }
     };
 
-    private BaseProduct.BaseProductListener mDJIBaseProductListener = new BaseProduct.BaseProductListener() {
-        @Override
-        public void onComponentChange(BaseProduct.ComponentKey key, BaseComponent oldComponent, BaseComponent newComponent) {
-            if(newComponent != null) {
-                newComponent.setComponentListener(mDJIComponentListener);
-            }
-            notifyStatusChange();
+    private void refreshSDKRelativeUI() {
+        BaseProduct mProduct = FindingApplication.getProductInstance();
+
+        if (null != mProduct && mProduct.isConnected()) {
+            Connected_View();
+        } else {
+            Disconnected_View();
         }
-        @Override
-        public void onConnectivityChange(boolean isConnected) {
-            notifyStatusChange();
-        }
-    };
-    private BaseComponent.ComponentListener mDJIComponentListener = new BaseComponent.ComponentListener() {
-        @Override
-        public void onConnectivityChange(boolean isConnected) {
-            notifyStatusChange();
-        }
-    };
-    private void notifyStatusChange() {
-        mHandler.removeCallbacks(updateRunnable);
-        mHandler.postDelayed(updateRunnable, 500);
     }
-    private Runnable updateRunnable = new Runnable() {
-        @Override
-        public void run() {
-            Intent intent = new Intent(FLAG_CONNECTION_CHANGE);
-            sendBroadcast(intent);
-        }
-    };
 
 
 }
